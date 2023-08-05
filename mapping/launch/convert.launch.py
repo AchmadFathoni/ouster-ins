43df1525375ee4c76217d86e859289948189e79f
Achmad Fathoni <fathoni.id@gmail.com>
2023-08-05 22:42:44 +0700

    Add convert.launch.py

---
+++ mapping/launch/convert.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+
+from ament_index_python.packages import get_package_share_path
+
+def generate_launch_description():
+    bag_file = LaunchConfiguration('bag_file')
+    lidar = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource([
+            os.path.join(get_package_share_path('ouster_ros'), 'launch/replay.composite.launch.xml')
+        ]),
+        launch_arguments={'bag_file': bag_file, 'viz': 'False'}.items()
+    )
+
+    record = ExecuteProcess(
+        cmd = ['ros2', 'bag', 'record', '/odom_ins_enu', '/ouster/points', '--disable-keyboard-controls'],
+        output = 'screen'
+    )
+
+    return LaunchDescription([
+        lidar, record
+    ])
+
