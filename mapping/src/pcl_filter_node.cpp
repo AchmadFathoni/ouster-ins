904b3ad214daf17155cdbec8126c18716dc68a71
Achmad Fathoni <fathoni.id@gmail.com>
2024-06-08 16:46:17 +0800

    Add topic params and small fix of pcl_filter

--- mapping/src/pcl_filter_node.cpp
+++ mapping/src/pcl_filter_node.cpp
@@ -6,29 +6,35 @@
 using sensor_msgs::msg::PointCloud2;
 using std::placeholders::_1;
 using Pointcloud = pcl::PointCloud<pcl::PointXYZ>;
-using std::sqrt, std::pow;
+using std::pow, std::string, std::map;
+
+const string node_name = "pcl_filter";
 
 class Filter : public rclcpp::Node {
 public:
-    Filter() : Node("pcl_filter") {
-        sub_ = this->create_subscription<PointCloud2>("/ouster/points", 1, std::bind(&Filter::callback, this, _1));
-        pub_ = this->create_publisher<PointCloud2>("/filtered", 1);
-        this->declare_parameter("min_depth", 0.9);
-        min_depth_ = this->get_parameter("min_depth").as_double();
+    Filter() : Node(node_name) {
+        this->declare_parameters(node_name, params_);
+        this->get_parameters(node_name, params_);
+        this->declare_parameter(node_name + ".min_depth", 0.9);
+        min_depth_square_ = pow(this->get_parameter(node_name + ".min_depth").as_double(), 2);
+
+        sub_ = this->create_subscription<PointCloud2>(params_["in_topic"], 1, std::bind(&Filter::callback, this, _1));
+        pub_ = this->create_publisher<PointCloud2>(params_["out_topic"], 1);
     }
 private:
     rclcpp::Publisher<PointCloud2>::SharedPtr pub_;
     rclcpp::Subscription<PointCloud2>::SharedPtr sub_;
-    double min_depth_;
+    map<string, string> params_{{"in_topic", "/ouster/points"}, {"out_topic", "/filtered"}};
+    double min_depth_square_;
     void callback(const PointCloud2::SharedPtr in_cloud) const {
         Pointcloud pcl_cloud;
         pcl::fromROSMsg(*in_cloud, pcl_cloud);
-        
-        auto& points = pcl_cloud.points; 
+
+        auto& points = pcl_cloud.points;
         for (std::size_t p = 0; p < pcl_cloud.points.size(); ++p) {
             auto& point = points[p];
-            auto depth = sqrt(pow(point.x, 2) + pow(point.y, 2) + pow(point.z, 2));
-            if (depth < min_depth_) {
+            auto depth_square = pow(point.x, 2) + pow(point.y, 2) + pow(point.z, 2);
+            if (depth_square < min_depth_square_) {
                 point = points[points.size() - 1];
                 points.resize(points.size() - 1);
                 --p;
