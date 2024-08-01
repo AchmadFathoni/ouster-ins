1fb59f6159cc66e6f2f2b3fdd1a0bc6db32be91b
Achmad Fathoni <fathoni.id@gmail.com>
2024-08-01 13:50:31 +0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";  # IMPORTANT!!!
+  };
+  outputs = { self, nix-ros-overlay, nixpkgs }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "Example project";
+          packages = with pkgs;[
+            colcon
+            pcl
+            jsoncpp
+            flatbuffers
+            (with rosPackages.humble; buildEnv {
+                paths = [
+                    ros-core
+                    ament-cmake-core
+                    python-cmake-module
+                    rviz2
+                    rqt-graph
+                    pcl-conversions
+                    tf2-eigen
+                ];
+            })
+          ];
+        };
+      });
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+}
